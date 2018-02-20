9aa0520997a20c0086353f5bdd9d7a6076597296
jellyforce <jelly.wie@gmail.com>
2018-02-20 17:16:11 +0100

    transformed progressbar to be a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'app';
+  bedrijfsnaam = 'bedrijfsnaam';
+  contactpersoon = 'Jelle Wiersma';
+  mail = 'Jelle.wie@gmail.com';
+  telefoon = '06 27 87 35 20';
+
+
+/*// progressbar values
+  current = 78;
+  max = 100;
+  stroke = 15;
+  radius = 125;
+  semicircle = false;
+  rounded = false;
+  responsive = true;
+  clockwise = true;
+  color = '#45ccce';
+  background = '#eaeaea';
+  duration = 1400;
+  animation = 'easeOutCubic';
+  animationDelay = 0;
+  animations = [];
+  gradient = false;
+  realCurrent = 0;
+
+
+  /!*handles text position (inside circle)*!/
+  getOverlayStyle() {
+    const isSemi = this.semicircle;
+    const transform = (isSemi ? '' : 'translateY(-50%) ') + 'translateX(-50%)';
+
+    return {
+      'top': isSemi ? 'auto' : '50%',
+      'bottom': isSemi ? '5%' : 'auto',
+      'left': '50%',
+      'transform': transform,
+      '-moz-transform': transform,
+      '-webkit-transform': transform,
+      'font-size': this.radius / 3.5 + 'px'
+    };
+  }
+
+
+  /!*function colors based on current value*!/
+
+
+  /!*current value of customer*!/*/
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+
+import { ProgressModule} from './progressbar/progress.module';
 
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
