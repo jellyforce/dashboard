a351cc9a24be1fc92f0d6df8a0bb3d2544a245b4
jellyforce <jelly.wie@gmail.com>
2018-02-20 10:57:17 +0100

    added modules to be used

--- src/app/shared/navigation/navigation.module.ts
+++ src/app/shared/navigation/navigation.module.ts
@@ -1,1 +1,15 @@
-/*test*/
+import { NgModule } from '@angular/core';
+
+
+
+
+@NgModule({
+  /**/
+  declarations: [],
+  imports:      [],
+  providers:    [],
+  bootstrap:    []
+})
+
+
+export class NavigationModule {}
